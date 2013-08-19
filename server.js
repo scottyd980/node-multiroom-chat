c2bf317e4240a8b8eb38c83a32f76a7fccc21cce
Scott Douglass <Scott@Scotts-MacBook-Pro.local>
2013-08-19 15:46:34 -0400

    fixed path

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.listen(3000, function() {
 	console.log("Server listening on port 3000.");
 });
 
-var chatServer = require('.lib/chat_server');
+var chatServer = require('./lib/chat_server');
 chatServer.listen(server);
 
 function send404(response) {
